e93dd98f8caa553c8ec8bae6f9d22d14b7ccdbeb
iamhiman <kashyap.himanshu27@rocketmail.com>
2021-09-30 20:59:20 +0530

    feat: Add left, right & search Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import { AccessTime, HelpOutline, Search } from "@mui/icons-material";
+import { Avatar } from "@mui/material";
 
 export const Header: FC = () => {
   return (
@@ -12,7 +13,7 @@ export const Header: FC = () => {
 
       <HeaderSearch>
         <Search />
-        <input placeholder="Search PAPAFAM" />
+        <input placeholder="Search Himanshu's Slack" />
       </HeaderSearch>
 
       <HeaderRight>
@@ -22,8 +23,65 @@ export const Header: FC = () => {
   );
 };
 
-const HeaderContainer = styled.div``;
-const HeaderLeft = styled.div``;
-const HeaderAvatar = styled.div``;
-const HeaderRight = styled.div``;
-const HeaderSearch = styled.div``;
+const HeaderContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: fixed;
+  width: 100%;
+  padding: 10px 0;
+  background-color: var(--slack-color);
+  color: white;
+`;
+
+const HeaderLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  align-items: flex-end;
+  flex: 0.3;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
+  }
+`;
+
+const HeaderSearch = styled.div`
+  display: flex;
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    background-color: transparent;
+    color: white;
+  }
+`;
